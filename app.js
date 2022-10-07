3c5de86344b1cabe92aaa8530158010d8d43647d
zekysf <115142623+Zekysf@users.noreply.github.com>
2022-10-07 00:48:19 -0300

    Deixando um salve no arquivo app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação esta se conectando ao banco de dados")
 console.log("Enviando para o github")
+console.log("Deixando um salve no nosso trabalho")
